68258cd34d7b2b7ce4ca6d24013e5cb6c7844e37
Antonio Garcia <egartru@gmail.com>
2017-09-13 08:36:01 +0100

    Working properly with h_m :through, missing show view

--- app/models/list_tag.rb
+++ app/models/list_tag.rb
@@ -2,7 +2,7 @@ class ListTag < ApplicationRecord
 		
 		#validates_presence_of :name
 
-		belongs_to :todo
+		belongs_to :todo  # here the , optional: :true option doesn't apply as this is a join table
 		belongs_to :tag
 
 		accepts_nested_attributes_for :tag, reject_if: :all_blank, allow_destroy: true
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
 
-	#validates_presence_of :name # This can be removed or make it optional: true. Rigth now is mandatory
+	#validates_presence_of :name -- > This can be removed or make it optional: true. Rigth now is mandatory
 
 	has_many :list_tags,  inverse_of: :tag
 	has_many :todos, :through => :list_tags
